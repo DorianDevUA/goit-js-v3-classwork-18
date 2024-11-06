affa1a55be9179bc69354fd050673697b35c7703
Dorian Devua <Dorian.DevUa@gmail.com>
2024-11-06 14:01:49 +0400

    finish classwork with Repeta

---
+++ src/js/01-promise-api.js
@@ -0,0 +1,62 @@
+import '../css/style.css';
+/*
+ * Создание промиса
+ *  - Класс Promise
+ *  - resolve
+ *  - reject
+ *  - Promise.prototype.then(onResolve, onReject)
+ */
+
+const promise = new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.5;
+  // if (canFulfill) {
+  //   resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+  // }
+
+  // reject('Промис выполнился с ошибкой (отклонён, rejected)');
+});
+
+console.log('MyPromise', promise);
+
+// const promise = new Promise((resolve, reject) => {
+//   const canFulfill = Math.random() > 0.5;
+
+//   setTimeout(() => {
+//     if (canFulfill) {
+//       resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+//     }
+
+//     reject('Промис выполнился с ошибкой (отклонён, rejected)');
+//   }, 1000);
+// });
+
+// // promise.then(onFulfilled, onRejected);
+
+// function onFulfilled(result) {
+//   console.log('onFulfilled -> onFulfilled');
+//   console.log(`✅ ${result}`);
+// }
+
+// function onRejected(error) {
+//   console.log('onRejected -> onRejected');
+//   console.log(`❌ ${error}`);
+// }
+
+// /*
+//  * Цепочки промисов (chaining)
+//  * Promise.prototype.catch(error)
+//  * Promise.prototype.finally()
+//  */
+
+// promise
+//   .then(onFulfilled)
+//   .then(x => {
+//     console.log(x);
+
+//     return 10;
+//   })
+//   .then(y => {
+//     console.log(y);
+//   })
+//   .catch(error => console.log(error))
+//   .finally(() => console.log('Я буду выполнен в любом случае'));
